ff2c395f2377ebbdf741de2d12fb4c390ee12c99
youratpapp <youratpapp@gmail.com>
2026-05-29 14:21:56 -0400

    fix: usar logo svg oficial

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -21,7 +21,7 @@ import {
 } from 'lucide-react'
 import { lazy, type FormEvent, Suspense, useEffect, useMemo, useRef, useState } from 'react'
 import './App.css'
-import capitalLogo from './assets/capital-truck-center-logo.jpg'
+import capitalLogo from './assets/capital-truck-center-logo.svg'
 import {
   clientes as seedClientes,
   alteracoes as seedAlteracoes,
